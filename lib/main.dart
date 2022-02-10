934b78385e609b550ad8a993f1dc6bc17da3b397
r00tIsRoot <35025164+r00tIsRoot@users.noreply.github.com>
2022-02-10 22:07:17 +0900

    - codingApple 강의 2강

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('앱임'),
+        ),
+
+        body:
+          Text('안녕'),
+
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 75,
+            child:
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(Icons.phone),
+                  Icon(Icons.message),
+                  Icon(Icons.contact_page),
+                ],
+              ),
+          )
+        ),
+      )
+    );
+  }
+}
